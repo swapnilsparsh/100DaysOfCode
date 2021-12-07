f530ee22f12c6a2eef18043ad98cc7337538d223
swapnilsparsh <arezona.lucky55@gmail.com>
2021-12-07 23:56:06 +0530

    Matrix Transpose

--- Day15/MatrixTranspose.cpp
+++ Day15/MatrixTranspose.cpp
@@ -1,18 +1,19 @@
 #include<iostream>
 using namespace std;
 int main(){
-  int n,m;
-  cin>>n>>m;
-  int array[n][m];
-  for(int i=0;i<n;i++){
-    for(int j=0;j<m;j++){
-      cin>>array[i][j];
+  int array[3][3] = {{1,2,3}, {4,5,6}, {7,8,9}};
+  for(int i=0;i<3;i++){
+    for(int j=i;j<3;j++){ //j=i 
+      int temp = array[i][j];
+      array[i][j] = array[j][i];
+      array[j][i] = temp; 
     }
   }
 
-  for(int i=0;i<n;i++){
-    for(int j=0;j<m;j++){
-      cout<<array[j][i]<<" ";
+  /*Output*/
+  for(int i=0;i<3;i++){
+    for(int j=0;j<3;j++){
+      cout<<array[i][j]<<" ";
     }
     cout<<endl;
   }
